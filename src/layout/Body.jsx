ba3cb4fba956a7f6845964fcfcc74da6192b62ce
IamAnk211997 <ankit.patnaik85@gmail.com>
2024-02-13 00:34:14 +0530

    bug fix added

--- src/layout/Body.jsx
+++ src/layout/Body.jsx
@@ -5,11 +5,12 @@ import * as Selector from '@selector';
 
 const Body = () => {
     const widgy = useSelector(Selector.widgetSelect.widgetData);
+    const charty = useSelector(Selector.graphSelect.graphData);
+    const tableData = useSelector(Selector.dataSelect.dataSelect);
 
-    console.log(widgy);
     return (
         <div>
-            {widgy.length != 0 ?
+            {widgy.length != 0 && charty.length != 0 && tableData.length!=0?
                 <Landing /> :
                 <div className="w-full h-[75vh] flex justify-center items-center">
                     <div className="flex items-center relative text-3xl font-bold">Please select a Value from DropDown</div>
