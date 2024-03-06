8c197e82b684ef8afc36da9b8a3b0ef3a72a851f
IamAnk211997 <ankit.patnaik85@gmail.com>
2024-03-06 16:36:03 +0530

    Preventing unwanted clicks

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -20,8 +20,14 @@ const Header = () => {
         <div className="container mx-auto px-4 py-6 flex justify-between items-center">
             <div className="flex justify-between gap-5">
               <div className="flex items-center relative text-3xl font-bold" onClick={() => navigateTo('/Landing')}>My Dashboard</div>
-              <div className="flex items-center relative text-2xl font-bold" onClick={() => navigateTo('/Analysis')}>Analysis</div>
-              <div className="flex items-center relative text-2xl font-bold" onClick={() => navigateTo('/Forecast')}>Forecast</div>
+              {
+                selectedItem!=''?
+                <>
+                  <div className="flex items-center relative text-2xl font-bold" onClick={() => navigateTo('/Analysis')}>Analysis</div>
+                  <div className="flex items-center relative text-2xl font-bold" onClick={() => navigateTo('/Forecast')}>Forecast</div>
+                </>:<></>
+              }
+
             </div>
             <div className="relative">
                 <Dropdown data={stockData} stateData={stateData} homeAction={() => navigateTo('/Landing')}/>
